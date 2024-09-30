e8b2dc52b8b972cf1b0e07f19d464aca3d20f585
Picaroon142 <picaroon142@gmail.com>
2024-10-01 00:06:29 +0300

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const QRCode = require('qrcode');
-const socketio = require('socket.io')
+const socketio = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = app.listen(3000);
 
 const io = socketio(server);
 
+
 app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 io.on('connection', (connectedSocket) => {
   connectedSocket.on('QR', async (data) => {
