2c17b71fa8c4bb8a980ecc9bab3975ef6902457b
Picaroon142 <picaroon142@gmail.com>
2024-09-30 23:55:27 +0300

    first commit

---
+++ public/index.js
@@ -0,0 +1,30 @@
+const socket = io.connect("")
+
+const text = document.getElementById('text');
+const sub = document.getElementById('sub');
+const img = document.getElementById('qrImage');
+
+function removeSpaces(str) {
+    return str.replace(/\s+/g, '');
+}
+
+sub.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    let res = removeSpaces(text.value);
+    if (res == '') {
+        alert('Please enter a valid URL.')
+    } else {
+        console.log('+')
+        socket.emit('QR', {content: text.value})
+    }
+})
+
+socket.on('QRResponse', (data) => {
+    if (data.error) {
+        console.error(data.error);
+        alert(data.error);
+    } else {
+        img.src = data.content;
+    }
+});
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const QRCode = require('qrcode');
+const socketio = require('socket.io')
+
+const app = express();
+const server = app.listen(3000);
+
+const io = socketio(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (connectedSocket) => {
+  connectedSocket.on('QR', async (data) => {
+    try {
+      const url = data.content;
+      const qrCodeImage = await QRCode.toDataURL(url);
+      connectedSocket.emit('QRResponse', { content: qrCodeImage });
+    } catch (error) {
+      console.error('Error generating QR code:', error);
+      connectedSocket.emit('QRResponse', { error: 'Error generating QR code' });
+    }
+  });
+});
